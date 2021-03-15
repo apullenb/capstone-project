defb70600cf0ef5e744d1e53d75c38e2cd84b127
Andrea Pullen <apullenb@gmail.com>
2021-03-15 14:15:22 -0500

    server issues

--- app.js
+++ app.js
@@ -33,12 +33,7 @@ app.use('/api/journal', journalRouter);
 app.use('/api/food', foodRouter);
 
 app.use((error, req, res, next) => {
-  let response;
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }};
-  } else {
-    response = { error };
-  }
+  let response = { error };
   res.status(500).json(response);
 });
 
