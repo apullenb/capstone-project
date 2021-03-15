1d4c8835e7203a9c4375dbf5f6349c02da7faba5
Andrea Pullen <apullenb@gmail.com>
2021-03-15 16:15:08 -0500

    pg

--- server.js
+++ server.js
@@ -7,25 +7,6 @@ const { DATABASE_URL } = require('./config')
 const PORT = process.env.PORT
 app.use(cors())
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
